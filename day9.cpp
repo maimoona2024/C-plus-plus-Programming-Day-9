7b1d1eeb3ddedb7060ea0823848684a6851a3114
Maimoona <160620135+maimoona2024@users.noreply.github.com>
2024-04-13 18:09:35 +0500

    Add files via upload

---
+++ day9.cpp
@@ -0,0 +1,173 @@
+// //Program # 1 of Day 9: Logical and Operator
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<"Please! Enter a number: ";
+//     cin>>n;
+//     if(n>99 and n<1000) {
+//         cout<<"Four digit number."<<endl;
+//     }
+//     else
+//     {
+//         cout<<"Not a four digit number."<<endl;
+//     }
+// }
+
+// //Program # 2: Logical && Operator
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<"Please! Enter a number: ";
+//     cin>>n;
+//     if(n>99 && n<1000) {
+//         cout<<"Four digit number."<<endl;
+//     }
+//     else
+//     {
+//         cout<<"Not a four digit number."<<endl;
+//     }
+// }
+
+// //Program # 3: Logical and operator.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int m;
+//     cout<<"Please! Enter any number: ";
+//     cin>>m;
+//     if(m%5==0 and m%3==0){
+//         cout<<"The number you have entered is divisible by both 5 and 3.";
+//     }
+//     else {
+//         cout<<"The number you have entered is not divisible by both 5 and 3.";
+//     }
+// }
+
+//Program # 4: Logical && operator.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int m;
+//     cout<<"Please! Enter any number: ";
+//     cin>>m;
+//     if(m%5==0 && m%3==0){
+//         cout<<"The number you have entered is divisible by both 5 and 3.";
+//     }
+//     else {
+//         cout<<"The number you have entered is not divisible by both 5 and  3.";
+//     }
+// }
+
+//Program # 5: Logical or operator
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int num;
+//     cout<<"Please Enter a number: ";
+//     cin>>num;
+//     if(num%5 == 0 or num%3==0) {
+//         cout<<"Number is divisible by 5 or 3: ";
+//     }
+//     else {
+//         cout<<"Number is not divisible by 5 or 3: ";
+//     }
+// }
+
+//Program # 6: Logical || Operator.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int num;
+//     cout<<"Please Enter a number: ";
+//     cin>>num;
+//     if(num%5 == 0 or num%3==0) {
+//         cout<<"Number is divisible by 5 or 3: ";
+//     }
+//     else {
+//         cout<<"Number is not divisible by 5 or 3: ";
+//     }
+// }
+
+
+//Program # 6: Program for proving sides of triangle
+
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int a;
+//     int b;
+//     int c;
+//     cout<<"Please! Enter first side of triangle: "<<endl;
+//     cin>>a;
+//     cout<<"Please! Enter second side of triangle: "<<endl;
+//     cin>>b;
+//     cout<<"Please! Enter third side of triangle: "<<endl;
+//     cin>>c;
+//     if((a+b)>c && (a+c)>b && (a+c)>b) {
+//         cout<<"These are sides of a triangle."<<endl;
+//     }
+//     else {
+//         cout<<"These are not sides of a triangle."<<endl;
+//     }
+// }
+
+//Program # 7: Program for finding greatest numbers between three numbers.
+
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int a,b,c;
+//     cout<<"Please Enter first positive number: "<<endl;
+//     cin>>a;
+//     cout<<"Please Enter second positive number: "<<endl;
+//     cin>>b;
+//     cout<<"Please Enter second positive number: "<<endl;
+//     cin>>c;
+//     if(a>b && a>c) {
+//         cout<<a<<" is greatest."<<endl;
+//     }
+//     else if(b>a && b>c) {
+//         cout<<b<<" is greatest."<<endl;
+//     }
+//     else if(c>b && c>a) {
+//         cout<<c<<" is greatest."<<endl;
+//     }
+//     else if(a==b==c) {
+//         cout<<a<<","<<b<<" and "<<c<<" are Equal.";
+//     }
+// }
+
+//Program # 7: Program for finding smallest numbers between three numbers.
+
+// #include<iostream>
+// using namespace std;
+// int main() {
+//     int a,b,c;
+//     cout<<"Please Enter first positive number: "<<endl;
+//     cin>>a;
+//     cout<<"Please Enter second positive number: "<<endl;
+//     cin>>b;
+//     cout<<"Please Enter second positive number: "<<endl;
+//     cin>>c;
+//     if(a<b && a<c) {
+//         cout<<a<<" is smallest."<<endl;
+//     }
+//     else if(b<a && b<c) {
+//         cout<<b<<" is smallest."<<endl;
+//     }
+//     else if(c<b && c<a) {
+//         cout<<c<<" is smallest."<<endl;
+//     }
+//     else if(a==b==c) {
+//         cout<<a<<","<<b<<" and "<<c<<" are Equal.";
+//     }
+// }
